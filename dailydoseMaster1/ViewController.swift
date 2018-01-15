4da19583c7a12daadd986f372395e8d8e6c865f6
studystudent <mmdasilva@thestudy.qc.ca>
2018-01-15 15:26:48 -0500

    first commit, home,register,login

---
+++ dailydoseMaster1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dailydoseMaster1
+//
+//  Created by Study Student on 2018-01-15.
+//  Copyright © 2018 Marcella Maria Da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
